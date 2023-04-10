efafcbdf0da1c4880e63d4a6d4cc99759cfb3045
Apoorva Kaushal <apoorvakaushal93@gmail.com>
2023-04-10 21:37:06 +0530

    Commit Done

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
 const express = require('express')
 const path = require('path')
 const ejs = require("ejs");
-
-// const bodyParser = require('body-parser')
-
 const multer  = require('multer')
-// const upload = multer({ dest: 'uploads/' })
 const bodyParser = require('body-parser')
+const auth = require('./middleware/auth')
 
 const app = express()
 const port = 3000
 const {collection, collection2,collection3} = require("./mongodb");
 const { param } = require('express-validator');
-
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-
-app.set('view engine', 'ejs')
-
-app.use(express.static(path.join(__dirname, "public")))
-
-app.use('/' , require(path.join(__dirname, 'routes/blog.js')))
-
-app.use(express.static(__dirname+"./public/"));
-
 var Storage = multer.diskStorage({
   destination: "./public/uploads/",
   filename:  (req, file, cb) =>{
@@ -33,20 +18,25 @@ var Storage = multer.diskStorage({
 var upload=multer({
   storage:Storage
 });
-// .array('file',12)
-// var Storage1 = multer.diskStorage({
-//   destination: "./public/propDoc/",
-//   filename:  (req, file, cb) =>{
-//     cb(null, file.fieldname + "_" + Date.now()+path.extname(file.originalname));
-//   }
-// });
-// var upload=multer({
-//   storage:Storage
-// }).single("pdfFile");
 
-// var upload=multer({
-//   storage:Storage
-// }).single('file');
+
+
+
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, "public")))
+app.use('/' , require(path.join(__dirname, 'routes/blog.js')))
+app.use(express.static(__dirname+"./public/"));
+app.use('/sell' , require(path.join(__dirname, 'routes/blog.js')))
+
+
+
+
+
+
 
 
 app.get('/flats',(req,res)=>  {
@@ -57,9 +47,6 @@ app.get('/flats',(req,res)=>  {
   })
 })
 
-
-//for uplaoding images
-
 app.get('/upload',(req,res)=>  {
   // var success=req.file.filename+" uplaod successfully";
   
@@ -69,6 +56,7 @@ app.get('/upload',(req,res)=>  {
     console.log(y)
   })
 })
+
 app.post('/upload',upload.single("file"),async (req,res)=>  {
   // console.log(image)
   const imagedetails={
@@ -84,12 +72,7 @@ app.post('/upload',upload.single("file"),async (req,res)=>  {
   res.render("upload")
 })
 
-
-
-
-
-app.use('/sell' , require(path.join(__dirname, 'routes/blog.js')))
-app.get('/sell',(req,res)=>  {
+app.get('/sell', auth, (req,res)=>  {
     res.render("sell", { route:'/sell'})
   
 })
@@ -124,7 +107,6 @@ app.get("/signup", function (req, res) {
   res.render("signup", {route : '/signup'});
 });
 
-// Handling user signup
 app.post('/signup', async(req, res) => {
   const data = {
     name: req.body.name,
@@ -135,21 +117,11 @@ app.post('/signup', async(req, res) => {
   await collection.insertMany([data]);
   res.render("home")
 })
-// app.post("/signup", async (req, res) => {
-//   const user = await collection.create({
-//     name: req.body.uname,
-//     email:req.body.email,
-//     password: req.body.password,
-//     contact:req.body.contact
-//   });
-//   res.send("User Sign up successfully")
-//   // res.status(200).json(user);
-// });
 
-//Showing login form
 app.get("/login", function (req, res) {
   res.render("login",{route:'/login'});
 });
+
 app.post("/login", async function(req, res){
   try {
       // check if the user exists
@@ -169,9 +141,6 @@ app.post("/login", async function(req, res){
       res.status(400).json({ error });
     }
 });
-// app.get("/adminlogin", function (req, res) {
-//   res.render("adminlogin", {route : '/adminlogin'});
-// });
 app.get('/rent', function(req, res){
   res.render("rent"), { route:'/rent' }
 })
@@ -179,22 +148,6 @@ app.get('/rent', function(req, res){
 app.get("/admin", function (req, res) {
   res.render("admin", {route : '/admin'});
 });
-
-
-
-
-
-
-
-
-
-
-// app.get('/flat/:id', (req, res) => {
-//   const flatId = req.params.id; // get the flat id parameter from the request
-//   // find the flat details using the flatId and pass them to the view
-//   const flat = findFlatDetails(flatId); // replace with your function to fetch the flat details
-//   res.render('flatDetails', { flat });
-// });
 
 app.get('/flats/:id',(req,res)=>  {
   collection2.findById(req.params.id).then((x) =>{
@@ -207,29 +160,10 @@ app.get('/flats/:id',(req,res)=>  {
 
 
 
+
+
+
+
 app.listen(process.env.PORT || port , () => {
     console.log('Listening at port https://localhost:${port}')
 })
-
-
-// function sendMail(){
-//   var params = {
-//     email: document.getElementById("emailHelp").value ,
-//     phone:document.getElementById("contactNumberField").value ,
-//     Query: document.getElementById("QueryField").value,
-//   };
-// const serviceID = "service_5vovegd";
-// const templateID = "template_3pbw70x"
-// emailjs.send(serviceID,templateID,params)
-// .then(
-//   res =>{
-//     document.getElementById("email").value="";
-//     document.getElementById("phone").value="";
-//     document.getElementById("Query").value="";
-//     console.log(res);
-//     alert("your message sent successfully");
-//   })
-// .catch((err) => console.log(err));
-
-
-// }
